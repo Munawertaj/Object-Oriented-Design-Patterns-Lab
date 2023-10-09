0ee8b45c96f08a46a23b91e711e7b77760dee6c3
Munawer Taj <taj4580@gmail.com>
2023-10-09 21:13:37 +0600

    Investment class added

---
+++ Assignment - 03/Investment.java
@@ -0,0 +1,37 @@
+public class Investment implements IAccount {
+    private int accountNumber;
+    private int balance;
+
+    public Investment(int accountNumber, int amount) {
+        this.accountNumber = accountNumber;
+        this.balance = amount;
+    }
+
+    @Override
+    public void deposit(int amount) {
+        balance += amount;
+    }
+
+    @Override
+    public void withdraw(int amount) {
+        if (balance >= amount)
+            balance -= amount;
+        else
+            System.out.println("Insufficient Balance!!!");
+    }
+
+    @Override
+    public void transfer(IAccount toAccount, int amount) {
+        withdraw(amount);
+        toAccount.deposit(amount);
+    }
+
+    @Override
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public int showBalance() {
+        return balance;
+    }
+}
