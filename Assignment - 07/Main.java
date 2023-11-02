41f37ba877f357d8d3ed2fdeb86e44d5620f5864
Munawer Taj <taj4580@gmail.com>
2023-11-02 21:48:42 +0600

    Updating Main class

--- Assignment - 07/Main.java
+++ Assignment - 07/Main.java
@@ -1,23 +1,43 @@
 public class Main {
     public static void main(String[] args) {
+        // Create a vending machine with an initial stock of 5 products
         VendingMachine vendingMachine = new VendingMachine(5);
 
-        vendingMachine.insertDollar();
-        vendingMachine.dispense();
-        vendingMachine.ejectMoney();
+        // Simulate various actions and state transitions
+        System.out.println("Vending Machine Simulation:");
 
-        vendingMachine.insertDollar();
-        vendingMachine.insertDollar();
-        vendingMachine.dispense();
-        vendingMachine.dispense();
+        // Initial state: Idle
+        System.out.println("Current State: Idle");
+        vendingMachine.insertDollar();  // Inserting a dollar
 
-        vendingMachine.insertDollar();
-        vendingMachine.dispense();
+        // State transition to HasOneDollarState
+        System.out.println("\nCurrent State: HasOneDollarState");
+        vendingMachine.dispense();  // Dispensing a product
+        vendingMachine.ejectMoney(); // Ejecting money
 
-        vendingMachine.insertDollar();
-        vendingMachine.dispense();
+        // State transition back to Idle
+        System.out.println("\nCurrent State: Idle");
+        vendingMachine.insertDollar();  // Inserting a dollar
 
-        vendingMachine.insertDollar();
-        vendingMachine.dispense();
+        // State transition to HasOneDollarState again
+        System.out.println("\nCurrent State: HasOneDollarState");
+        vendingMachine.insertDollar();  // Inserting another dollar
+        vendingMachine.dispense();  // Dispensing a product
+
+        // State transition to OutOfStockState
+        System.out.println("\nCurrent State: OutOfStockState");
+        vendingMachine.dispense();  // Attempting to dispense (out of stock)
+
+        // State remains OutOfStockState
+        System.out.println("\nCurrent State: OutOfStockState");
+        vendingMachine.insertDollar();  // Attempting to insert a dollar
+
+        // State remains OutOfStockState
+        System.out.println("\nCurrent State: OutOfStockState");
+        vendingMachine.ejectMoney();  // Attempting to eject money
+
+        // Note: You can continue testing other scenarios and state transitions here.
+
+        System.out.println("\nSimulation Complete.");
     }
 }
